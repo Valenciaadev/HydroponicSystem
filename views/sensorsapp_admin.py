157daec605a311f3e6794d616c925cf5c9a0e971
dylan <dpaez@ucol.mx>
2025-04-25 01:14:18 -0600

    se cambió el texto por la primer caja

--- views/sensorsapp_admin.py
+++ views/sensorsapp_admin.py
@@ -1,13 +1,28 @@
-from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QFrame
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont
 
 class SensorsAppAdmin(QWidget):
     def __init__(self, ventana_login, embed=False):
         super().__init__()
+        
         layout = QVBoxLayout()
-        label = QLabel("Pantalla de Sensores (Dylan)")
-        label.setAlignment(Qt.AlignCenter)
-        label.setFont(QFont("Candara", 16))
-        layout.addWidget(label)
+        
+        box = QFrame()
+        box.setFixedSize(600, 500) #for mi when i will changig size, first is the WIDTH and then go the Height
+        box.setStyleSheet("""
+            QFrame {
+        background-color: #2C6E63;  /* color entre azul marino y verde */
+        border-radius: 10px;
+        border: 2px solid #1E4D45;
+            }
+        """)
+        
+        layout.addWidget(box, alignment=Qt.AlignCenter)
         self.setLayout(layout)
+        
+        
+    def testSlot(self):
+        print("Hola mundo")
+        
+        
